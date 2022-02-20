fb4e01abc57d4891825a2e45f37bfb15aad564ae
Gabriele Petronella <gabriele@buildo.io>
2022-02-20 13:09:35 +0100

    Header layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,9 +29,18 @@ const Header = () => {
 const MainHeader = styled.div`
   padding: 0 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: center;
+  height: 72px;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  justify-content: space-evenly;
+  gap: 48px;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
